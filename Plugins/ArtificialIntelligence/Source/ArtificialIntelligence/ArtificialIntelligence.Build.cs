1294fa9a0aeb8c252e3a3b90ea3adfad9d8c58c0
Yves Tanas <Yves.Tanas@gmx.de>
2020-10-24 23:05:51 +0200

    Update ArtificialIntelligence.Build.cs

--- Plugins/ArtificialIntelligence/Source/ArtificialIntelligence/ArtificialIntelligence.Build.cs
+++ Plugins/ArtificialIntelligence/Source/ArtificialIntelligence/ArtificialIntelligence.Build.cs
@@ -38,6 +38,7 @@ public class ArtificialIntelligence : ModuleRules
 				"Engine",
 				"Slate",
 				"SlateCore",
+				"AIModule",
 				// ... add private dependencies that you statically link with here ...	
 			}
 			);
